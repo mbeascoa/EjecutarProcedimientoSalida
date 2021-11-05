0985924d4e47452f822bab0387f3a4c5b0983148
mbeascoa <miguel.beascoa@beastek.net>
2021-11-05 16:44:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+import cx_Oracle
+
+connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
+
+cursor = connection.cursor()
+try:
+
+    dp = input("Número de departamento:")
+    loc = cursor.var(cx_Oracle.STRING)
+
+    args = (loc, dp)
+    cursor.callproc('DevolverLocalidad', args)
+    print(loc.getvalue())
+
+except connection.Error as error:
+    print("Error: ", error)
+cursor.close()
+connection.close()
